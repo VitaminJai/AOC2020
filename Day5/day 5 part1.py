e704568f4df763eb9e7d8c95e0dce531a76df61f
VitaminJai <vir.jai123@live.co.uk>
2020-12-05 12:45:57 +0000

    Day5

--- Day5/day 5 part1.py
+++ Day5/day 5 part1.py
@@ -1,9 +1,9 @@
 myFile = open("seats.txt","r")
 content = myFile.read().split("\n")
-newlist = []
-
+idlist = []
 trans = str.maketrans('FBRL', '0110')
 
 for line in content:
-    newlist.append(int(line.translate(trans), 2))
-print(max(newlist))
+    idlist.append(int(line.translate(trans), 2))
+
+print(max(idlist))
---
+++ Day5/day5 part2.py
@@ -0,0 +1,16 @@
+myFile = open("seats.txt","r")
+content = myFile.read().split("\n")
+idlist = []
+trans = str.maketrans('FBRL', '0110')
+
+for lines in content:
+    idlist.append(int(lines.translate(trans), 2))
+
+idsort = sorted(idlist)
+
+for i in range(idsort[0], idsort[-1]):
+    if i not in idsort:
+        print(i)
+
+
+
